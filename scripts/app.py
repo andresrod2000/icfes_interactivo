42f29cc948e4708b9c3a80c49b23d644309d8140
Nicolas Mantilla <nicolas2210707@correo.uis.edu.co>
2024-07-20 17:33:28 -0500

    consultas saber pro

--- scripts/app.py
+++ scripts/app.py
@@ -11,10 +11,14 @@ CORS(app)
 
 PUNTAJES_SABER11 = ['punt_ingles', 'punt_matematicas', 'punt_sociales_ciudadanas', 'punt_c_naturales', 'punt_lectura_critica', 'punt_global']
 PROMEDIOS_SABER11 = ['promedio_ingles', 'promedio_matematicas', 'promedio_sociales_ciudadanas', 'promedio_c_naturales', 'promedio_lectura_critica', 'promedio_global']
-PUNTAJES_SABERPRO = ['']
+PUNTAJES_SABERPRO = ['punt_ingles', 'punt_razona_cuantitativo', 'punt_c_ciudadana', 'punt_lectura_critica', 'punt_comuni_escrita']
+PROMEDIOS_SABERPRO = ['promedio_ingles', 'promedio_razona_cuantitativo', 'promedio_c_ciudadana', 'promedio_lectura_critica', 'promedio_comuni_escrita']
 
 app.consulta_inicial = None
 app.consulta_municipio = None
+
+app.consulta_inicial_pro = None
+app.consulta_municipio_pro = None
 
 # <---------------- Estaticos del inicio ---------------->
 @app.route('/')
@@ -189,7 +193,102 @@ def consulta_estrato():
         return jsonify({"error": "No se ha cargado la consulta por municipio"}), 400
 
 
-# <------------------------ Saber Pro ---------------------------->
+# <---------------------------- Saber Pro ---------------------------->
+
+# <---------------- Consulta inicial por departamento y rango ---------------->
+
+@app.route('/saberpro/consulta_inicial/DELETE')
+def delete_consulta_inicial_pro():
+    app.consulta_inicial_pro = None
+    return 'Consulta inicial eliminada', 200
+
+@app.route('/saberpro/consulta_inicial')
+def consulta_departamento_pro():
+    departamento = request.args.get('departamento', type=str)
+    start = request.args.get('start', type=int)
+    end = request.args.get('end', type=int)
+    
+    if not all([departamento, start, end]):
+        return jsonify({"error": "Faltan parámetros requeridos"}), 400
+
+    try:
+        results = consultas.get_consulta_departamento_pro(departamento, start, end)
+        
+        # Convertir los resultados a un DataFrame y guardarlo en el contexto de la aplicación
+        app.consulta_inicial_pro = pd.DataFrame(results)
+
+        # Reiniciar la consulta por municipio
+        app.consulta_municipio_pro = None
+        
+        return f'Consulta exitosa, size de la consulta: {app.consulta_inicial_pro.size}', 200
+    
+    except Exception as e:
+        print(f"Error en consulta_departamento_pro: {str(e)}")
+        return jsonify({"error": f"Error al realizar la consulta: {str(e)}"}), 500
+    
+@app.route('/saberpro/consulta_inicial/test', methods=['GET'])
+def check_dataframe_pro():
+    df = app.consulta_inicial_pro
+    info = {
+        "is_empty": df.empty,
+        "shape": df.shape if not df.empty else None,
+        "columns": df.columns.tolist() if not df.empty else None,
+        "sample": df.head().to_dict(orient='records') if not df.empty else None
+    }
+    return jsonify(info), 200
+
+@app.route('/saberpro/consulta_inicial/municipios')
+def get_municipios_pro():
+    if app.consulta_inicial_pro is None:
+        return jsonify({"error": "No se ha cargado la consulta inicial"}), 400
+    df = app.consulta_inicial_pro.copy()
+    municipios = df['ESTU_INST_MUNICIPIO'].unique().tolist()
+    return municipios
+
+# <-------------------- Consulta por municipio ------------------------->
+
+@app.route('/saberpro/consulta_municipio')
+def consulta_municipio_pro():
+    municipio = request.args.get('municipio', type=str)
+    if not municipio:
+        return jsonify({"error": "Falta el parámetro 'municipio'}"}), 400
+
+    df = app.consulta_inicial_pro.copy()
+    df = df[df['ESTU_INST_MUNICIPIO'] == municipio]
+    df.drop(columns=['ESTU_INST_MUNICIPIO'], inplace=True)
+    app.consulta_municipio_pro = df
+    return f'Consulta exitosa, size de la consulta: {app.consulta_municipio_pro.size}', 200
+
+@app.route('/saberpro/consulta_municipio/test', methods=['GET'])
+def check_dataframe_municipio_pro():
+    df = app.consulta_municipio_pro
+    info = {
+        "is_empty": df.empty,
+        "shape": df.shape if not df.empty else None,
+        "columns": df.columns.tolist() if not df.empty else None,
+        "sample": df.head().to_dict(orient='records') if not df.empty else None
+    }
+    return jsonify(info), 200
+
+# <--------------------- Consultas generales (estados) --------------------->
+@app.route('/saberpro/consultas/estrato')
+def consulta_estrato_pro():
+    municipio = request.args.get('municipio', type=str)
+
+    if not municipio:
+        df = app.consulta_inicial_pro.copy()
+        df = df[['FAMI_ESTRATOVIVIENDA', *PUNTAJES_SABERPRO]].groupby('FAMI_ESTRATOVIVIENDA', as_index=False).mean().round(2).sort_values('FAMI_ESTRATOVIVIENDA')
+        df.columns = ['Estado', *PROMEDIOS_SABERPRO]
+        return df.to_json(orient='records'), 200
+
+    elif app.consulta_municipio_pro is not None:
+        df = app.consulta_municipio_pro.copy()
+        df = df[['FAMI_ESTRATOVIVIENDA', *PUNTAJES_SABERPRO]].groupby('FAMI_ESTRATOVIVIENDA', as_index=False).mean().round(2).sort_values('FAMI_ESTRATOVIVIENDA')
+        df.columns = ['Estado', *PROMEDIOS_SABERPRO]
+        return df.to_json(orient='records'), 200
+    
+    else:
+        return jsonify({"error": "No se ha cargado la consulta por municipio"}), 400
 
 
 
